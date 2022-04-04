8455c8f02f687af11e67adb468fc9b831cf525ab
Anish kumar <94589270+Anishkumar31@users.noreply.github.com>
2022-04-04 18:56:56 +0000

    p5

--- p5original.c
+++ p5original.c
@@ -1,34 +1,53 @@
-#include<stdio.h>
-int input_arrray_size(){
+#include <stdio.h>
+
+int input_array_size()
+{
   int n;
-  printf("Enter the array size : ");
-  scanf("%d",&n);
-  return n;
+  printf("Enter the number till which you want to find the prime numbers\n");
+  scanf("%d", &n);
+  return n + 1;
 }
-void init_array(int n,int a[n]){
-  for(int i=0;i<n;i++){
-    scanf("%d",i);
-  }
-}
-void erostosthenes_sieve(int n,int a[n]){
-  for(int i=0;i<n;i++){
-    d=a[i];
-    sqr=a[i]*a[i];
-    while(a[i]<=sqr){
-      if(a[i]%d==0){
-        a[i]=0;
-      }
+
+void init_array(int n, int a[n])
+{
+  a[0] = 0;
+  a[1] = 0;
+  for (int i = 2; i<= n; i++)
+    {
+      a[i] = i;
     }
-  }
 }
-void out_put(int n,int a[n]){
-  
+
+void erotosthenes_sieve(int n, int a[n])
+{
+  for (int i = 2; i < n; i++)
+    {
+        for (int k = i + i; k < n && a[i]; k += i)
+         {
+            a[k] = 0;
+          }
+    }
 }
-int main(){
-  int n;
-  n = input_array_size();
-  int a[n];
-  init_array(n,a);
-  erotosthenes_sieve(n,a);
-  out_put(n,a);
+
+void output(int n, int a[n])
+{
+  for (int i = 0; i < n - 1; i++)
+    {
+      if (a[i])
+        printf("%d,", a[i]);
+    }
+  if (a[n-1])
+    printf("%d\n", a[n-1]);
+  else
+    printf("\n");
+}
+
+int main()
+{
+  int size = input_array_size();
+  int array[size];
+  init_array(size, array);
+  erotosthenes_sieve(size, array);
+  output(size, array);
+  return 0;
 }
